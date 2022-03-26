f0c7fd4e957c783a6a66c12686f7e2eaf343bb50
didos-re <didos-re@student.42sp.org.br>
2022-03-26 22:18:09 +0100

    second commit

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -1,16 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: didos-re <didos-re@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/25 16:33:56 by didos-re          #+#    #+#             */
-/*   Updated: 2022/03/25 21:02:57 by didos-re         ###   ########.fr       */
+/*   Created: 2022/03/25 21:27:52 by didos-re          #+#    #+#             */
+/*   Updated: 2022/03/26 17:39:35 by didos-re         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+int	ft_iterative_factorial(int nb)
 {
-	*nbr = 42;
+	int	f;
+
+	if (nb <= 12 && nb > 0)
+	{
+		f = 1;
+		while (nb >= 1)
+		{
+			f = f * nb;
+			nb--;
+		}
+		return (f);
+	}
+	else if (nb == 0)
+		return (1);
+	else
+		return (0);
 }
--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -1,16 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: didos-re <didos-re@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/25 16:33:56 by didos-re          #+#    #+#             */
-/*   Updated: 2022/03/25 21:02:57 by didos-re         ###   ########.fr       */
+/*   Created: 2022/03/26 05:26:16 by didos-re          #+#    #+#             */
+/*   Updated: 2022/03/26 17:40:02 by didos-re         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
-{
-	*nbr = 42;
+int	ft_recursive_factorial(int nb)
+{	
+	if (nb == 1 || nb == 0)
+		return (1);
+	else if (nb <= 12 && nb > 1)
+		return (nb * ft_recursive_factorial(nb - 1));
+	else
+		return (0);
 }
