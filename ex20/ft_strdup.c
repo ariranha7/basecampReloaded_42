54d4f0adf7ddc361dd3f070a9495e17c6924aa5e
ariranha7 <avdiegoreis@hotmail.com>
2022-03-31 18:07:43 +0200

    ninth commit

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -6,36 +6,25 @@
 /*   By: didos-re <didos-re@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 19:19:59 by didos-re          #+#    #+#             */
-/*   Updated: 2022/03/31 17:14:06 by didos-re         ###   ########.fr       */
+/*   Updated: 2022/03/31 17:49:25 by didos-re         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-char    *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
-    char    *ptr;
-    int        i;
-    
-    i = 0;
-    while (src[i])
-    {
-        i++;
-    }
-    ptr = (char *) malloc((i + 1) * sizeof(char));
-    while (i >= 0)
-    {
-      ptr[i] = src[i];
-      i--;
-    }
-    //free(ptr);
-    return (ptr);
-    //free(ptr);
-}
+	char	*ptr;
+	int		i;
 
-int    main()
-{
-    char    nome[] = "MARYSTELA";
-    printf("%s \n", ft_strdup(nome));
+	i = 0;
+	while (src[i])
+	{
+		i++;
+	}
+	ptr = (char *) malloc((i + 1) * sizeof(char));
+	while (i >= 0)
+	{
+		ptr[i] = src[i];
+		i--;
+	}
+	return (ptr);
 }
