975c3d83256476de1c79fd0bd0835ebed3f6b805
ariranha7 <avdiegoreis@hotmail.com>
2022-04-01 04:22:58 +0200

    tenth commit

--- ex22/ft_abs.h
+++ ex22/ft_abs.h
@@ -1,28 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: didos-re <didos-re@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/31 17:19:28 by didos-re          #+#    #+#             */
-/*   Updated: 2022/04/01 02:32:20 by didos-re         ###   ########.fr       */
+/*   Created: 2022/03/31 21:52:33 by didos-re          #+#    #+#             */
+/*   Updated: 2022/04/01 04:20:29 by didos-re         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	*ft_range(int min, int max)
-{
-	int	*num;
-	int	i;
-
-	if (min >= max)
-		return (num = '\0');
-	num = (int *) malloc((max - min) * sizeof(int));
-	i = 0;
-	while (i < (max - min))
-	{
-		num[i] = min + i;
-		i++;
-	}
-	return (num);
-}
+#define 
--- rascunho.c
+++ rascunho.c
@@ -1,14 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   rascunho.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: didos-re <didos-re@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/31 17:19:28 by didos-re          #+#    #+#             */
-/*   Updated: 2022/04/01 02:32:20 by didos-re         ###   ########.fr       */
+/*   Created: 2022/03/31 20:20:41 by didos-re          #+#    #+#             */
+/*   Updated: 2022/03/31 21:47:43 by didos-re         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int	*ft_range(int min, int max)
 {
@@ -17,6 +20,7 @@ int	*ft_range(int min, int max)
 
 	if (min >= max)
 		return (num = '\0');
+
 	num = (int *) malloc((max - min) * sizeof(int));
 	i = 0;
 	while (i < (max - min))
@@ -25,4 +29,21 @@ int	*ft_range(int min, int max)
 		i++;
 	}
 	return (num);
+}
+
+int	main()
+{
+	int	min;
+	int max;
+	int	i;
+	
+	min = 50;
+	max = 100;
+	i = 0;
+	while (i < (max - min))
+	{
+		printf("%d, ", ft_range(min, max)[i]);
+		i++;
+	}
+	printf("\n");
 }
